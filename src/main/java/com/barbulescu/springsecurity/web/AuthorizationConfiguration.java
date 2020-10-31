d7097a43a194defe65e4e39a3117dd6bf0b5c2cb
Marius Barbulescu <marius.barbulescu@gmail.com>
2020-10-31 06:23:12 +0100

    matcher with **

--- src/main/java/com/barbulescu/springsecurity/web/AuthorizationConfiguration.java
+++ src/main/java/com/barbulescu/springsecurity/web/AuthorizationConfiguration.java
@@ -19,24 +19,9 @@ public class AuthorizationConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic();
 
-        //        http.formLogin()
-//                .defaultSuccessUrl("/main", true);
-
-//        http.authorizeRequests()
-//                .mvcMatchers("/hello/*").hasRole("ADMIN")
-//                .mvcMatchers("/ciao/*").hasRole("MANAGER")
-//                .anyRequest()
-//                .hasAnyAuthority("READ", "WRITE")
-//                .access("hasAuthority('READ') and !hasAuthority('DELETE')")
-//                .access("T(java.time.LocalTime).now().isAfter(T(java.time.LocalTime).of(12, 0))")
-//                .hasRole("ADMIN")
-//                .authenticated()
-
         http.authorizeRequests()
-                .mvcMatchers(HttpMethod.GET, "/a").authenticated()
-                .mvcMatchers(HttpMethod.POST, "/a").permitAll()
-                .anyRequest()
-                .denyAll();
+                .mvcMatchers(HttpMethod.GET, "/a/b/**").authenticated()
+                .anyRequest().permitAll();
 
         http.csrf().disable();
     }
