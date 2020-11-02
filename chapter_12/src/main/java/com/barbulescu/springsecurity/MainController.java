069db02159552efde23c65042382bc14ce24336a
Marius Barbulescu <marius.barbulescu@gmail.com>
2020-11-02 08:33:25 +0100

    chapter 12 - get user details

--- chapter_12/src/main/java/com/barbulescu/springsecurity/MainController.java
+++ chapter_12/src/main/java/com/barbulescu/springsecurity/MainController.java
@@ -1,13 +1,17 @@
 package com.barbulescu.springsecurity;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Log4j2
 public class MainController {
 
     @GetMapping("/")
-    public String main() {
+    public String main(OAuth2AuthenticationToken token) {
+        log.info(String.valueOf(token.getPrincipal()));
         return "main.html";
     }
 
