2170bb0f653a803b00f2ea71de8c72a855a84e2b
Marius Barbulescu <marius.barbulescu@gmail.com>
2020-11-04 08:20:19 +0100

    chapter 16 - test method security

--- chapter_16/src/test/java/com/barbulescu/springsecurity/MainTests.java
+++ chapter_16/src/test/java/com/barbulescu/springsecurity/MainTests.java
@@ -1,12 +1,48 @@
 package com.barbulescu.springsecurity;
 
+import com.barbulescu.springsecurity.hello.NameService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.test.context.support.WithMockUser;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 @SpringBootTest
 class MainTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Autowired
+    private NameService nameService;
+
+    @Test
+    @DisplayName("When the method is called without an authenticated user, it throws AuthenticationException")
+    void testNameServiceWithNoUser() {
+        assertThatExceptionOfType(AuthenticationException.class)
+                .isThrownBy(() -> nameService.getName());
+    }
+
+    @Test
+    @WithMockUser(authorities = "read")
+    @DisplayName("When the method is called with an authenticated user having a wrong authority, it throws AccessDeniedException")
+    void testNameServiceWithUserButWrongAuthority() {
+        assertThatExceptionOfType(AccessDeniedException.class)
+                .isThrownBy(() -> nameService.getName());
+    }
+
+    @Test
+    @WithMockUser(authorities = "write")
+    @DisplayName("When the method is called with an authenticated user having a correct authority, it returns the expected result")
+    void testNameServiceWithUserButCorrectAuthority() {
+        var result = nameService.getName();
+
+        assertThat(result).isEqualTo("Fantastico");
     }
 }
